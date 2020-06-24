a7173aa67bf5adb32c40dce0945f57306af83da7
aditya3131 <singhaditya3116@gmail.com>
2020-06-24 14:02:27 +0530

    overview completed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const url =require("url");
 
 
-/////////////////////////////
+/////////////////////////////////////////////
 //FILE
 
 // // fs.readFile("./txt/start.txt","utf-8",(err,data)=>{
@@ -23,8 +23,29 @@ const url =require("url");
 //     })
 // })
 
-//////////////////////////////
+//////////////////////////////////////////////////
 //SERVER
+const replaceTemplate = (temp,product)=>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%ID%}/g,product.id);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+
+    if(product.organic == false)
+    {
+        output = output.replace(/{%NOT_ORGANIC%}/g,"not-organic");
+    }
+    return output;
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");  //overview page
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");       // product page
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");          //card page
 
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,"utf-8")       //Tis code run only once i.e.synchronous
 const dataObj = JSON.parse(data)  //convert JSON to JS object
@@ -32,11 +53,23 @@ const dataObj = JSON.parse(data)  //convert JSON to JS object
 const server=http.createServer((req,res)=>{ //This is the code which run always
 
     const pathName = req.url;
+
+    //Overview page
     if(pathName =="/" || pathName == "/overview")
     {
-        res.end("THis is from overview");
+        res.writeHead(200,{'Content-type':'text/html' })
+
+        const cardHtml = dataObj.map((element)=>{return replaceTemplate(tempCard,element);}).join("");
+        console.log(cardHtml);
+
+        let overview = tempOverview.replace(/{%CARD%}/g,cardHtml);
+        res.end(overview);
+
+    //Product Page
     }else if(pathName == "/product"){
         res.end("This is from product");
+
+    //Api Page
     }
     else if(pathName == "/api")
     {
